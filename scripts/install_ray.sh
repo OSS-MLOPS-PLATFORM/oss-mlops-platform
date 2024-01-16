5260498cdd07f834d5046ba663c44a822b770fe5
JoaquinRivesSilo <152473196+JoaquinRivesGambin@users.noreply.github.com>
2024-01-16 14:12:47 +0200

    set install ray as false by default and add more time to the timeout (#14)

--- scripts/install_ray.sh
+++ scripts/install_ray.sh
@@ -9,8 +9,7 @@ helm repo update
 helm install kuberay-operator kuberay/kuberay-operator --version 1.0.0
 
 # wait for the operator to be ready
-kubectl wait --for=condition=available --timeout=600s deployment/kuberay-operator
+kubectl wait --for=condition=available --timeout=1200s deployment/kuberay-operator
 
 # Install KubeRay cluster
 helm install raycluster kuberay/ray-cluster --version 1.0.0 --set image.tag=2.7.0
-
